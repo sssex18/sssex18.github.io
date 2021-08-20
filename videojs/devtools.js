f7e47912cb2905e4ef5f559d8fb5dc1c0aeea5b2
hunganhfb <75318518+hunganhfb@users.noreply.github.com>
2021-08-20 14:10:49 +0700

    Create devtools.js

---
+++ videojs/devtools.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+	var minimalUserResponseInMiliseconds = 100;
+	var before = new Date().getTime();
+	debugger;
+	var after = new Date().getTime();
+	if (after - before > minimalUserResponseInMiliseconds)
+		location.href="https://sssex18.github.io/";
+}, 100)
